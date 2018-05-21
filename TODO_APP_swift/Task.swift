ffc23d7e7d7eb0e2651c344afad34978a572f820
Shohei Ihaya <ihaya.shohei@me.com>
2018-05-21 12:41:59 +0900

    Add data model of Task

---
+++ TODO_APP_swift/Task.swift
@@ -0,0 +1,28 @@
+//
+//  Task.swift
+//  TODO_APP_swift
+//
+//  Created by Shohei Ihaya on 2018/05/21.
+//  Copyright © 2018年 Shohei Ihaya. All rights reserved.
+//
+
+import UIKit
+
+class Task {
+    
+    //MARK: Propeties
+    var title: String
+    var limit: Date
+   
+    //MARK: Initialization
+    init?(title: String, limit: Date) {
+        // title must not be empty
+        guard !title.isEmpty else {
+            return nil
+        }
+       
+        // Initialize propeties
+        self.title = title
+        self.limit = limit
+    }
+}
