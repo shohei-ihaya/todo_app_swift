f7876549f7db4b7d1a9bc8950e869859d338d7fe
Shohei Ihaya <ihaya.shohei@me.com>
2018-05-23 12:03:49 +0900

    Implement task detail scene with task title and task limit

--- TODO_APP_swift/TaskViewController.swift
+++ TODO_APP_swift/TaskViewController.swift
@@ -8,12 +8,78 @@
 
 import UIKit
 
-class TaskViewController: UIViewController {
+class TaskViewController: UIViewController, UITextFieldDelegate {
+
+    //MARK: Propeties
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var limitTextField: UITextField!
+
+    var datePicker = UIDatePicker()
+    var toolBar = UIToolbar()
+
+    @IBOutlet weak var datePickerContainerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        titleTextField.delegate = self
+        limitTextField.delegate = self
+
+        setupDatePicker()
+        setupUIToolBar()
+        limitTextField.inputView = datePicker
+        limitTextField.inputAccessoryView = toolBar
+
+    }
+
+    //MARK: datePicer
+    func setupDatePicker() {
+        datePicker.addTarget(self, action: #selector(self.datePickerValueChanged(datePicker:)), for: .valueChanged)
+    }
+
+    @objc func datePickerValueChanged(datePicker: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        limitTextField.text = dateFormatter.string(from: datePicker.date)
+    }
+
+    //MARK: UIToolbar
+    func setupUIToolBar() {
+        toolBar = UIToolbar(frame: CGRect(x:0, y:0, width: view.frame.width, height: 40))
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed(sender:)))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        toolBar.setItems([flexibleSpace, doneButton], animated: true)
+    }
+
+    @objc func doneButtonPressed(sender: UIBarButtonItem) {
+        limitTextField.resignFirstResponder()
+    }
+
+    // MARK: - UITextFieldDelegate
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case titleTextField:
+            print("title")
+        case limitTextField:
+            print("title")
+        default:
+            fatalError("Unexpected textField is edited. \(textField)")
+        }
+        // Disable the Save button while editiong.
+        //saveButton.isEnabled = false
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        titleTextField.resignFirstResponder()
+        limitTextField.resignFirstResponder()
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //updateSaveButtonState()
+        //navigationItem.title = textField.text
     }
 
     override func didReceiveMemoryWarning() {
