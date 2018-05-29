f8e574efacb1816f56005d1bc9c42dab998f8579
Shohei Ihaya <ihaya.shohei@me.com>
2018-05-29 12:53:43 +0900

    Remove unneccesary file

--- TODO_APP_swift/uiview_first_responder.swift
+++
@@ -1,9 +0,0 @@
-//
-//  uiview_first_responder.swift
-//  TODO_APP_swift
-//
-//  Created by Shohei Ihaya on 2018/05/22.
-//  Copyright © 2018年 Shohei Ihaya. All rights reserved.
-//
-
-import Foundation
